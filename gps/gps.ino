3a780aeb388d19152ebb23c3c6bc46efe85fb3bd
Arthur <arthurvianna@id.uff.br>
2022-09-05 18:06:41 -0300

    writing gps data to file

--- gps/gps.ino
+++ gps/gps.ino
@@ -1,10 +1,16 @@
-#include<SoftwareSerial.h>    
-#include<TinyGPS.h>           
+#include<SoftwareSerial.h>
+#include<TinyGPS.h>
+#include <SD.h>
 
-SoftwareSerial SerialGPS(8, 9);     
-TinyGPS GPS;                       
+// CMD allow monitor serial in linux
+// sudo chmod a+rw /dev/ttyACM0
 
-float lat, lon, speed;
+SoftwareSerial SerialGPS(8, 9);
+TinyGPS GPS;
+File gps_data_file;
+
+
+float lat, lon;//, speed;
 unsigned long date, hour;
 unsigned short sat;
 char ts[32]; // data to be stored
@@ -15,8 +21,37 @@ char speed_str[8];
 
 void setup() {
 
-  SerialGPS.begin(9600);  
-  Serial.begin(9600);     
+  SerialGPS.begin(9600);
+  Serial.begin(9600);
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+
+  Serial.println("SD initialization done.");
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+//  gps_data_file = SD.open("gps_data.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+//  if (gps_data_file) {
+//    Serial.print("Writing to test.txt...");
+//    gps_data_file.println("This is a test file :)");
+//    gps_data_file.println("testing 1, 2, 3.");
+//    for (int i = 0; i < 20; i++) {
+//      gps_data_file.println(i);
+//    }
+//    // close the file:
+//    gps_data_file.close();
+//    Serial.println("done.");
+//  }
+//  else {
+//    // if the file didn't open, print an error:
+//    Serial.println("error opening test.txt");
+//    while(1);
+//  }
 
   Serial.println("Buscando satelites...");
 }
@@ -35,13 +70,26 @@ void loop() {
 
       GPS.f_get_position(&lat, &lon);
 
-      speed = GPS.f_speed_kmph();
+      //speed = GPS.f_speed_kmph();
 
       dtostrf(lat, 2, 6, lat_str);
       dtostrf(lon, 2, 6, lon_str);
-      dtostrf(speed, 3, 2, speed_str);
+      dtostrf(GPS.f_speed_kmph(), 3, 2, speed_str);
       sprintf(data, "{\"ts\": %s, \"lat\": %s, \"lon\": %s, \"speed\": %s}", ts, lat_str, lon_str, speed_str);
-      Serial.println(data);
+      //Serial.println(data);
+      
+      // writing to file
+      gps_data_file = SD.open("gps_data.txt", FILE_WRITE);
+      if (gps_data_file) {
+        Serial.print("Writing data to file...");
+        gps_data_file.println(data);
+        gps_data_file.close();
+        Serial.println("Done.");
+      }
+      else {
+        Serial.println("error opening data file");
+      }
+      
             
 
       //Satelites
@@ -52,8 +100,7 @@ void loop() {
         Serial.println(sat);
       }
 
-      Serial.println("");
-      delay(60000); // 1 min delay
+      delay(9000); // aprox 10 sec for each gps reading
     }
   }
 }
